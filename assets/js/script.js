8a90f805b51592931149992d33a795334a45daa1
jvega <jvega@example.com>
2022-04-09 22:24:14 -0400

    Carga inicial de archivos

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//Jquery FIle
+//alert("entre al javascript");
+
+$(document).ready(function()
+{
+
+    //Activar memsaje cuando se realice Click en boton Enviar Correo
+    $('#enviarCorreo').click(function() {
+        alert("El correo fue enviado correctamente...");
+      });
+
+}); //cierre del $(docuemnt)
