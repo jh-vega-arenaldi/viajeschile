c3e580540637fb18682f72b404733b1e757a8be6
jvega <jvega@example.com>
2022-04-09 23:16:50 -0400

    se cambian los efectos en seccion quienes somos

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,12 @@
 $(document).ready(function()
 {
 
-//resaltar el texto al pasar el mouse sobre el texto de quienes somos
-  $( "#resalta-qs1" ).hover(function() {$( this ).addClass( "text-primary" );}, function() {$( this ).removeClass( "text-danger" );})
-  $( "#resalta-qs2" ).hover(function() {$( this ).addClass( "text-success" );}, function() {$( this ).removeClass( "text-danger" );})
-  $( "#resalta-qs3" ).hover(function() {$( this ).addClass( "text-warning" );}, function() {$( this ).removeClass( "text-danger" );})
+//resaltar el icono al pasar el mouse sobre cada seccion de quienes somos
+  $( "#resaltaqs1" ).hover(function() {$( this ).addClass( "text-danger" );}, function() {$( this ).removeClass( "text-danger" );})
+
+  $( "#resaltaqs2" ).hover(function() {$( this ).addClass( "text-danger" );}, function() {$( this ).removeClass( "text-danger" );})
+
+  $( "#resaltaqs3" ).hover(function() {$( this ).addClass( "text-danger" );}, function() {$( this ).removeClass( "text-danger" );})
 
     //Activar memsaje cuando se realice Click en boton Enviar Correo
     $('#enviarCorreo').click(function() {
