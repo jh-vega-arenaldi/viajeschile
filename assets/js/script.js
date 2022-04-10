fc29daf540386319c1634fd30dc2c3e0dee7c556
jvega <jvega@example.com>
2022-04-09 22:36:25 -0400

    modificar css para soportar el cambio de color de texto en quines somos al pasar el mouse por el texto

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,11 @@
 $(document).ready(function()
 {
 
+//resaltar el texto al pasar el mouse sobre el texto de quienes somos
+  $( "#resalta-qs1" ).hover(function() {$( this ).addClass( "text-primary" );}, function() {$( this ).removeClass( "text-danger" );})
+  $( "#resalta-qs2" ).hover(function() {$( this ).addClass( "text-success" );}, function() {$( this ).removeClass( "text-danger" );})
+  $( "#resalta-qs3" ).hover(function() {$( this ).addClass( "text-warning" );}, function() {$( this ).removeClass( "text-danger" );})
+
     //Activar memsaje cuando se realice Click en boton Enviar Correo
     $('#enviarCorreo').click(function() {
         alert("El correo fue enviado correctamente...");
